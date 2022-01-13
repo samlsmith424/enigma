62249db73f112af36be47276b7bd3862f2b02595
Sam Smith <samlsmith424@gmail.com>
2022-01-13 12:57:54 -0800

    Feat: #make_number_key and #key_shift

---
+++ lib/enigma.rb
@@ -0,0 +1,20 @@
+class Enigma
+  attr_reader :character_set
+
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
+
+  def make_number_key
+    5.times.map{rand(10)}.join
+  end
+
+  def key_shift(number)
+    numbers = []
+    numbers << number[0..1].to_i
+    numbers << number[1..2].to_i
+    numbers << number[2..3].to_i
+    numbers << number[3..4].to_i
+    numbers
+  end
+end
