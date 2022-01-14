e8c71ffadf73ec637d67866a0c7b622e84d496ef
Sam Smith <samlsmith424@gmail.com>
2022-01-14 14:28:43 -0800

    Feat: #encrypt with #final_shift taking arguments

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,12 @@
 class Enigma
   attr_reader :character_set,
-              :number_key
+              :number_key,
+              :date_key
 
-  def initialize(key = nil)
+  def initialize(key = nil, date = nil)
     @character_set = ("a".."z").to_a << " "
     @number_key = key || make_number_key
-
+    @date_key = date || current_date
   end
 
   def make_number_key
@@ -26,15 +27,15 @@ class Enigma
     Date.today.strftime("%d%m%y")
   end
 
-  def offset_shift
+  def offset_shift(date)
     # shift = current_date.to_i ** 2
     shift = date.to_i ** 2
     shift.to_s[-4..-1].split('').map(&:to_i)
   end
 
-  def final_shift
+  def final_shift(number_key, date_key) 
     keys = key_shift(number_key)
-    offsets = offset_shift
+    offsets = offset_shift(date_key)
     shifts = []
     shifts << keys[0] + offsets[0]
     shifts << keys[1] + offsets[1]
@@ -43,19 +44,20 @@ class Enigma
     shifts
   end
 
-  def encrypt(string, key, date = current_date)
+  def encrypt(string, key, date)
     hash = {:encryption => "", :key => key, :date => date}
     letters = string.split(//)
     letters.each_with_index do |character, index|
-      if index % 4 == 0
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift[0]) % 27))
+      if @character_set.include?(character) != true
+        hash[:encryption] += character
+      elsif index % 4 == 0
+        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[0]) % 27))
       elsif index % 4 == 1
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift[1]) % 27))
+        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[1]) % 27))
       elsif index % 4 == 2
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift[2]) % 27))
+        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[2]) % 27))
       elsif index % 4 == 3
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift[3]) % 27))
-        # require "pry"; binding.pry
+        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[3]) % 27))
       end
     end
     hash
