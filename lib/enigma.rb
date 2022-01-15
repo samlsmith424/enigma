5f153c5501036dae57c9134b51d41f362bdb9268
Sam Smith <samlsmith424@gmail.com>
2022-01-14 16:02:01 -0800

    Feat: refactor file to only include #initialize, #encrypt, and #decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,5 @@
 class Enigma
+  include Helpers
   attr_reader :character_set,
               :number_key,
               :date_key
@@ -7,41 +8,6 @@ class Enigma
     @character_set = ("a".."z").to_a << " "
     @number_key = key || make_number_key
     @date_key = date || current_date
-  end
-
-  def make_number_key
-    5.times.map{rand(10)}.join
-  end
-
-  def key_shift(key)
-    numbers = []
-    numbers << key[0..1].to_i
-    numbers << key[1..2].to_i
-    numbers << key[2..3].to_i
-    numbers << key[3..4].to_i
-    numbers
-  end
-
-  def current_date
-    # Time.now.strftime("%d/%m/%y").gsub('/', "")
-    Date.today.strftime("%d%m%y")
-  end
-
-  def offset_shift(date)
-    # shift = current_date.to_i ** 2
-    shift = date.to_i ** 2
-    shift.to_s[-4..-1].split('').map(&:to_i)
-  end
-
-  def final_shift(number_key, date_key)
-    keys = key_shift(number_key)
-    offsets = offset_shift(date_key)
-    shifts = []
-    shifts << keys[0] + offsets[0]
-    shifts << keys[1] + offsets[1]
-    shifts << keys[2] + offsets[2]
-    shifts << keys[3] + offsets[3]
-    shifts
   end
 
   def encrypt(string, key = number_key, date = date_key)
@@ -82,5 +48,4 @@ class Enigma
     end
     hash
   end
-
 end
