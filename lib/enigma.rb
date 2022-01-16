482866bffacfe10d7e0cc4670462304a304397c9
Sam Smith <samlsmith424@gmail.com>
2022-01-16 12:21:01 -0800

    Feat: make hash variable name more clear

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,41 +12,41 @@ class Enigma
   end
 
   def encrypt(string, key = number_key, date = date_key)
-    hash = {:encryption => "", :key => key, :date => date}
+    encrypted = {:encryption => "", :key => key, :date => date}
     letters = string.split(//)
     letters.each_with_index do |character, index|
       character.downcase!
       if @character_set.include?(character) != true
-        hash[:encryption] += character
+        encrypted[:encryption] += character
       elsif index % 4 == 0
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[0]) % 27))
+        encrypted[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[0]) % 27))
       elsif index % 4 == 1
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[1]) % 27))
+        encrypted[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[1]) % 27))
       elsif index % 4 == 2
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[2]) % 27))
+        encrypted[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[2]) % 27))
       elsif index % 4 == 3
-        hash[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[3]) % 27))
+        encrypted[:encryption] += @character_set.fetch(((@character_set.index(character) + final_shift(key, date)[3]) % 27))
       end
     end
-    hash
+    encrypted
   end
 
   def decrypt(string, key, date = date_key)
-    hash = {:decryption => "", :key => key, :date => date}
+    decrypted = {:decryption => "", :key => key, :date => date}
     letters = string.split(//)
     letters.each_with_index do |character, index|
       if @character_set.include?(character) != true
-        hash[:decryption] += character
+        decrypted[:decryption] += character
       elsif index % 4 == 0
-        hash[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[0]) % 27))
+        decrypted[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[0]) % 27))
       elsif index % 4 == 1
-        hash[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[1]) % 27))
+        decrypted[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[1]) % 27))
       elsif index % 4 == 2
-        hash[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[2]) % 27))
+        decrypted[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[2]) % 27))
       elsif index % 4 == 3
-        hash[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[3]) % 27))
+        decrypted[:decryption] += @character_set.fetch(((@character_set.index(character) - final_shift(key, date)[3]) % 27))
       end
     end
-    hash
+    decrypted
   end
 end
